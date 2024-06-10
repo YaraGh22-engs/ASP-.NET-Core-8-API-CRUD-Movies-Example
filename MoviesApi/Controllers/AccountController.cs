aeebdeca5a5519493ed1882f09acf1ca49474493
Yara M Esmaiel <yaraesmaiel123@gmail.com>
2024-06-11 00:43:58 +0300

    Genrate JWT Token

--- MoviesApi/Controllers/AccountController.cs
+++ MoviesApi/Controllers/AccountController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace MoviesApi.Controllers
 {
@@ -9,9 +13,11 @@ namespace MoviesApi.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
-        public AccountController(UserManager<AppUser> userManager)
+        private readonly IConfiguration configuration;
+        public AccountController(UserManager<AppUser> userManager, IConfiguration configuration)
         {
             _userManager = userManager;
+            this.configuration = configuration;
         }
         [HttpPost("Register")]
         public async Task<IActionResult> RegisterNewUser(dtoNewUser dtouser)
@@ -50,7 +56,33 @@ namespace MoviesApi.Controllers
                 {
                     if (await _userManager.CheckPasswordAsync(user, dtolog.password))
                     {
-                        return Ok("Token");
+                        var claims = new List<Claim>();
+                        //claims.Add(new Claim("name", "value"));
+                        claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+                        claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id));
+                        claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+                        var roles = await _userManager.GetRolesAsync(user);
+                        foreach (var role in roles)
+                        {
+                            claims.Add(new Claim(ClaimTypes.Role, role.ToString()));
+                        }
+                        //signingCredentials
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecretKey"]));
+                        var sc = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                        var token = new JwtSecurityToken(
+                            claims: claims,
+                            issuer: configuration["JWT:Issuer"],
+                            audience: configuration["JWT:Audience"],
+                            expires: DateTime.Now.AddHours(1),
+                            signingCredentials: sc
+                            );
+                        var _token = new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(token),
+                            expiration = token.ValidTo,
+                        };
+                        return Ok(_token);
+
                     }
                     else
                     {
--- MoviesApi/Program.cs
+++ MoviesApi/Program.cs
@@ -87,6 +87,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseHttpsRedirection();
 
 
